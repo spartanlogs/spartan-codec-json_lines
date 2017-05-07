41c51dd0b1e9209c73c67a79ebc8db7d97226e90
Lee Keitel <lee@keitel.xyz>
2017-05-06 21:16:39 -0500

    Accept options

--- jsonLines.go
+++ jsonLines.go
@@ -5,25 +5,54 @@ import (
 	"io"
 
 	"github.com/spartanlogs/spartan/codecs"
+	"github.com/spartanlogs/spartan/config"
 	"github.com/spartanlogs/spartan/event"
+	"github.com/spartanlogs/spartan/utils"
 )
 
+type jsonLineConfig struct {
+	delimiter string
+}
+
+var jsonLineConfigSchema = []config.Setting{
+	{
+		Name:    "delimiter",
+		Type:    config.String,
+		Default: "\n",
+	},
+}
+
 // The JSONLineCodec encodes/decodes an event as JSON.
-type JSONLineCodec struct{}
+type JSONLineCodec struct {
+	config *jsonLineConfig
+}
 
 func init() {
 	codecs.Register("json_lines", newJSONLineCodec)
 }
 
-func newJSONLineCodec() (codecs.Codec, error) {
-	return &JSONLineCodec{}, nil
+func newJSONLineCodec(options utils.InterfaceMap) (codecs.Codec, error) {
+	c := &JSONLineCodec{
+		config: &jsonLineConfig{},
+	}
+	return c, c.setConfig(options)
+}
+
+func (c *JSONLineCodec) setConfig(options utils.InterfaceMap) error {
+	if err := config.VerifySettings(options, jsonLineConfigSchema); err != nil {
+		return err
+	}
+
+	c.config.delimiter = options.Get("delimiter").(string)
+
+	return nil
 }
 
 // Encode Event as JSON object.
 func (c *JSONLineCodec) Encode(e *event.Event) []byte {
 	data := e.Data()
 	j, _ := json.Marshal(data)
-	return append(j, '\n')
+	return append(j, []byte(c.config.delimiter)...)
 }
 
 // EncodeWriter reads events from in and writes them to w
