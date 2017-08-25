8c1e93376ec92c89e64e35bf7f07067579ae89e5
Lee Keitel <lee@keitel.xyz>
2017-08-24 20:40:58 -0500

    Use base codec

--- jsonLines.go
+++ jsonLines.go
@@ -2,7 +2,6 @@ package codecs
 
 import (
 	"encoding/json"
-	"io"
 
 	"github.com/spartanlogs/spartan/codecs"
 	"github.com/spartanlogs/spartan/config"
@@ -24,6 +23,7 @@ var jsonLineConfigSchema = []config.Setting{
 
 // The JSONLineCodec encodes/decodes an event as JSON.
 type JSONLineCodec struct {
+	codecs.BaseCodec
 	config *jsonLineConfig
 }
 
@@ -57,13 +57,7 @@ func (c *JSONLineCodec) Encode(e *event.Event) []byte {
 	return append(j, []byte(c.config.delimiter)...)
 }
 
-// EncodeWriter reads events from in and writes them to w
-func (c *JSONLineCodec) EncodeWriter(w io.Writer, in <-chan *event.Event) {}
-
 // Decode byte slice into an Event. CURRENTLY NOT IMPLEMENTED.
 func (c *JSONLineCodec) Decode(data []byte) (*event.Event, error) {
 	return nil, nil
 }
-
-// DecodeReader reads from r and creates an event sent to out
-func (c *JSONLineCodec) DecodeReader(r io.Reader, out chan<- *event.Event) {}
